b8fe3be64d55360ee39884d190408cd32e861f99
Shiqi Wang <thisisshiki@outlook.com>
2024-11-26 04:07:06 +1100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/odin-weather-app/sw.js', { scope: '/odin-weather-app/' })})}
